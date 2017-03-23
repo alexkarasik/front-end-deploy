9255d0efa9f009a138600c1888b220d94e898000
kinkorea <alexkarasik89@gmail.com>
2017-03-23 19:36:55 -0400

    Initial commit to set up this repository

---
+++ app/adapters/application.js
@@ -0,0 +1,6 @@
+import ENV from 'front-end-deploy/config/environment';
+import ActiveModelAdapter from 'active-model-adapter';
+
+export default ActiveModelAdapter.extend({
+  host: ENV.apiHost,
+});
